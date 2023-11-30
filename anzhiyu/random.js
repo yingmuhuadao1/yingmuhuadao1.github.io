1067e56fba4d0c288c845cdb9d729cd112ab25bc
“yingmuhuadao1” <2799676601@qq.com>
2023-11-30 19:45:16 +0800

    Site updated: 2023-11-30 19:45:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/29/hexo博客搭建/","2023/11/30/typora基本语法/","2023/11/30/用kali制作钓鱼页面/"];function toRandomPost(){
+var posts=["2023/11/29/hexo博客搭建/","2023/11/30/typora基本语法/","2023/11/30/用kali制作钓鱼页面/","2023/11/30/远控实验/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
